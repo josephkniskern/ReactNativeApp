4eea2e52609df6dc6c9c9e564fb46bd270296d9b
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-09-30 16:13:57 -0400

    new commit

--- App.js
+++ App.js
@@ -1,26 +1,41 @@
-import { StatusBar } from "expo-status-bar";
+
 import React from "react";
-import { StyleSheet, SafeAreaView, Dimensions, View } from "react-native";
+import { View } from "react-native";
+
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+
       <View style={{
-        backgroundColor: "dodgerblue",
-        width: 150,
-        height: 70
+        backgroundColor: "white",
+        flexDirection: "row",
+        justifyContent: "center"
       }}>
 
-      </View>
-    </SafeAreaView>
-  );
+        <View style={{
+          backgroundColor: "chocolate",
+          width: 100,
+          height: 100
+        }}></View>
+        <View style={{
+          backgroundColor: "darkkhaki",
+          width: 100,
+          height: 100
+        }}></View>
+        <View style={{
+          backgroundColor: "darkgoldenrod",
+          width: 100,
+          height: 100
+        }}></View>
+      </View>  
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "white",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
