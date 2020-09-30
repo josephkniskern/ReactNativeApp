b822d211934471ef405e808d56f6d0a3d9a10786
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-09-30 16:57:16 -0400

    welcome screen commit

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
 
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 
 export default function App() {
-  return (
-
-      <View style={{
-        backgroundColor: "white",
-        flexDirection: "row",
-        justifyContent: "center"
-      }}>
-
-        <View style={{
-          backgroundColor: "chocolate",
-          width: 100,
-          height: 100
-        }}></View>
-        <View style={{
-          backgroundColor: "darkkhaki",
-          width: 100,
-          height: 100
-        }}></View>
-        <View style={{
-          backgroundColor: "darkgoldenrod",
-          width: 100,
-          height: 100
-        }}></View>
-      </View>  
-  )
+  return <WelcomeScreen></WelcomeScreen>  
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "white",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
