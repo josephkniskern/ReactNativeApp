db29cf77019472ccb952f4b0c94fc38b70880d51
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-01 23:02:03 -0400

    first styling commit

--- App.js
+++ App.js
@@ -6,6 +6,20 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 
 export default function App() {
-  return <ViewImageScreen></ViewImageScreen>  
+  return (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+  >
+    <View style={{
+      backgroundColor: "dodgerblue",
+      width: 100,
+      height: 100
+    }}></View>
+  </View> 
+  ) 
 }
 
